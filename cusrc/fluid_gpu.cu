e7d967f6627cd377559f1e3dd8fe93905a1fe5a2
Mikołaj Matałowski <mikimikimat@gmail.com>
2025-12-08 17:50:37 +0100

    Part of GPU simulation implementation

--- cusrc/fluid_gpu.cu
+++ cusrc/fluid_gpu.cu
@@ -10,11 +10,73 @@
     }
 
 // clang-format off
-__global__ static void handle_bounderies_device(cudaSurfaceObject_t x_surf, int N)
-{
 
+/*
+Intended to use with small blocks, 128-256 threads
+to maximize occupancy (big blocks - lower granularity
+for SM allocation), but not to make scheduler overhead
+to large (small blocks like 32 threads - 1 warp - mean
+increase scheduler overhead)
+*/
+__global__ static void handle_rho_bounderies_device(cudaSurfaceObject_t rho_surf, int N)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i <= N){
+        float x_1_i = surf2Dread<float>(rho_surf, 1, i);
+        float x_N_i = surf2Dread<float>(rho_surf, N, i);
+        float y_i_1 = surf2Dread<float>(rho_surf, i, 1);
+        float y_i_N = surf2Dread<float>(rho_surf, i, N);
+
+        surf2Dwrite<float>(x_1_i, rho_surf, 0, i);
+        surf2Dwrite<float>(x_N_i, rho_surf, N, i);
+        surf2Dwrite<float>(y_i_1, rho_surf, i, 0);
+        surf2Dwrite<float>(y_i_N, rho_surf, i, N);
+    }
 }
 
+__global__ static void handle_u_bounderies_device(cudaSurfaceObject_t u_surf, int N)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i <= N){
+        float x_1_i = surf2Dread<float>(u_surf, 1, i);
+        float x_N_i = surf2Dread<float>(u_surf, N, i);
+        float y_i_1 = surf2Dread<float>(u_surf, i, 1);
+        float y_i_N = surf2Dread<float>(u_surf, i, N);
+
+        surf2Dwrite<float>(-x_1_i, u_surf, 0, i);
+        surf2Dwrite<float>(-x_N_i, u_surf, N, i);
+        surf2Dwrite<float>(y_i_1, u_surf, i, 0);
+        surf2Dwrite<float>(y_i_N, u_surf, i, N);
+    }
+}
+
+__global__ static void handle_v_bounderies_device(cudaSurfaceObject_t v_surf, int N)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i <= N){
+        float x_1_i = surf2Dread<float>(v_surf, 1, i);
+        float x_N_i = surf2Dread<float>(v_surf, N, i);
+        float y_i_1 = surf2Dread<float>(v_surf, i, 1);
+        float y_i_N = surf2Dread<float>(v_surf, i, N);
+
+        surf2Dwrite<float>(x_1_i, v_surf, 0, i);
+        surf2Dwrite<float>(x_N_i, v_surf, N, i);
+        surf2Dwrite<float>(-y_i_1, v_surf, i, 0);
+        surf2Dwrite<float>(-y_i_N, v_surf, i, N);
+    }
+}
+
+/*
+Intended to use with square tiles of threads and biggest
+possible thread blocks - this uses surfaces for better 
+caching (2D spacial locality) and to use shared memory
+as much as possible, threads will gather memory from 
+surface to shared memory (into L1) cache and the use those
+for computation.
+*/
 __global__ static void laplace_eq_solver_step_device(
     cudaSurfaceObject_t x_next_surf,
     cudaSurfaceObject_t x_surf,
@@ -59,6 +121,91 @@ __global__ static void laplace_eq_solver_step_device(
         surf2Dwrite<float>(val, x_next_surf, i * sizeof(float), j, cudaSurfaceBoundaryMode::cudaBoundaryModeZero);
     }
 }
+
+__device__ static int clamp_device(int x, int min, int max)
+{
+    if(x < min) return min;
+    if(x > max) return max;
+    return x;
+}
+
+__device__ static float bilerp_device(
+    cudaSurfaceObject_t src_surf,
+    float x, float y,
+    int N, float h)
+{
+    float halfh = 0.5f * h;
+    float xmax = N * h - halfh;
+    float ymax = N * h - halfh;
+
+    if (x < halfh)
+        x = halfh;
+    if (x > xmax)
+        x = xmax;
+    if (y < halfh)
+        y = halfh;
+    if (y > ymax)
+        y = ymax;
+
+    float gx = x / h;
+    float gy = y / h;
+
+    int i0 = (int)floor(gx); // base index
+    int j0 = (int)floor(gy);
+    int i1 = i0 + 1;
+    int j1 = j0 + 1;
+
+    // fractional parts
+    float sx = gx - i0;
+    float sy = gy - j0;
+
+    i0 = clamp_device(i0, 0, (int)N + 1);
+    i1 = clamp_device(i1, 0, (int)N + 1);
+    j0 = clamp_device(j0, 0, (int)N + 1);
+    j1 = clamp_device(j1, 0, (int)N + 1);
+
+    float f00 = surf2Dread<float>(src_surf, i0, j0);
+    float f01 = surf2Dread<float>(src_surf, i0, j1);
+    float f10 = surf2Dread<float>(src_surf, i1, j0);
+    float f11 = surf2Dread<float>(src_surf, i1, j1);
+
+    float res =
+        (1.0 - sx) * (1.0 - sy) * f00 +
+        (1.0 - sx) * sy * f01 +
+        sx * (1.0 - sy) * f10 +
+        sx * sy * f11;
+    
+    return res;
+}
+
+/* 
+Not much we can use here as for shared memory in this case.
+We can cannot predict memory location to sample from so we
+cannot cache this in shared memory. There is not much to 
+leverage from surface locality.
+*/
+__global__ static void advect_field_device(
+    cudaSurfaceObject_t u_surf,
+    cudaSurfaceObject_t v_surf,
+    cudaSurfaceObject_t source_surf,
+    cudaSurfaceObject_t dest_surf,
+    int N, float h, float dt)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x + 1;
+    int j = blockIdx.y * blockDim.y + threadIdx.y + 1;
+
+    if(i <= N && j <= N){
+        float u = surf2Dread<float>(u_surf, i, j);
+        float v = surf2Dread<float>(v_surf, i, j);
+
+        float x = i * h - dt * N * u;
+        float y = j * h - dt * N * v;
+
+        float val = bilerp_device(source_surf, x, y, N, h);
+
+        surf2Dwrite<float>(val, dest_surf, i * sizeof(float), j, cudaSurfaceBoundaryMode::cudaBoundaryModeZero);
+    }
+}
 // clang-format on
 
 FluidGpu::FluidGpu(size_t N)
@@ -70,27 +217,116 @@ FluidGpu::FluidGpu(size_t N)
 
     channel_desc = cudaCreateChannelDesc<float>();
 
-    prepare_surface(&density_desc, &density_surf, density_arr);
-    prepare_surface(&u_desc, &u_surf, u_arr);
-    prepare_surface(&v_desc, &v_surf, v_arr);
+    err = GetLastError();
+    CUDA_ERR_CHECK(err);
 
-    prepare_surface(&temp_desc, &temp_surf, temp_arr);
+    prepare_surface(&density_desc, &density_surf, &density_arr);
+    prepare_surface(&u_desc, &u_surf, &u_arr);
+    prepare_surface(&v_desc, &v_surf, &v_arr);
 
-    prepare_surface(&divergence_desc, &divergence_surf, divergence_arr);
-    prepare_surface(&p_desc, &p_surf, p_arr);
+    prepare_surface(&x_desc, &x_surf, &x_arr);
+    prepare_surface(&x_new_desc, &x_new_surf, &x_new_arr);
+
+    prepare_surface(&divergence_desc, &divergence_surf, &divergence_arr);
+    prepare_surface(&p_desc, &p_surf, &p_arr);
 }
 
-void FluidGpu::solve_laplace_eq_JA_solver(cudaSurfaceObject_t x_new, cudaSurfaceObject_t x, cudaSurfaceObject_t b, float a, float c)
+void FluidGpu::diffuse_density(float dt)
+{
+    float a = diff * dt * (1.0 / h) * (1.0 / h);
+
+    solve_laplace_eq_JA_solver(temp_surf, p_surf, density_surf, a, 1.0 + 4.0 * a, BoundaryHandleEnum::HandleRho);
+
+    /* New density surface is stored in coresponding x surface*/
+    std::swap(x_surf, density_surf);
+}
+
+void FluidGpu::diffuse_velocity(float dt)
+{
+    float a = visc * dt * (1.0 / h) * (1.0 / h);
+
+    solve_laplace_eq_JA_solver(x_new_surf, x_surf, u_surf, a, 1.0 + 4.0 * a, BoundaryHandleEnum::HandleRho);
+    /* New u surface is stored in coresponding x surface*/
+    std::swap(x_surf, u_surf);
+
+    solve_laplace_eq_JA_solver(x_new_surf, x_surf, v_surf, a, 1.0 + 4.0 * a, BoundaryHandleEnum::HandleRho);
+    /* New u surface is stored in coresponding x surface*/
+    std::swap(x_surf, v_surf);
+}
+
+void FluidGpu::advect_density(float dt)
 {
     cudaError_t err;
     int blocks_per_axis = (N / 32) + (N % 32 ? 1 : 0);
     dim3 blocks(blocks_per_axis, blocks_per_axis);
 
+    advect_field_device<<<blocks, dim3(32, 32)>>>(u_surf, v_surf, density_surf, x_surf, N, h, dt);
+
+    err = cudaGetLastError();
+    CUDA_ERR_CHECK(err);
+
+    std::swap(x_surf, density_surf);
+}
+
+void FluidGpu::advect_velocity(float dt)
+{
+    cudaError_t err;
+    int blocks_per_axis = (N / 32) + (N % 32 ? 1 : 0);
+    dim3 blocks(blocks_per_axis, blocks_per_axis);
+
+    advect_field_device<<<blocks, dim(32, 32)>>>(u_surf, v_surf, u_surf, x_surf, N, h, dt);
+
+    err = cudaGetLastError();
+    CUDA_ERR_CHECK(err);
+
+    advect_field_device<<<blocks, dim(32, 32)>>>(u_surf, v_surf, v_surf, x_new_surf, N, h, dt);
+
+    err = cudaGetLastError();
+    CUDA_ERR_CHECK(err);
+
+    std::swap(x_surf, u_surf);
+    std::swap(x_new_surf, v_surf);
+
+    /* !!! HANDLE BOUNDARY CONDITION !!! */
+}
+
+void FluidGpu::solve_laplace_eq_JA_solver(cudaSurfaceObject_t x_new, cudaSurfaceObject_t x, cudaSurfaceObject_t b, float a, float c, enum BoundaryHandleEnum e)
+{
+    cudaError_t err;
+    int blocks_per_axis = (N / 32) + (N % 32 ? 1 : 0);
+    dim3 blocks(blocks_per_axis, blocks_per_axis);
+
+    int mod = ((N + 2) * (N + 2)) % 128;
+    int blc_bd_task = ((N + 2) * (N + 2)) / 128 + (mod != 0 ? 1 : 0);
+    dim3 blocks_for_bd_task(blc_bd_task);
+
     for (int iter = 0; iter < 20; iter++)
     {
         laplace_eq_solver_step_device<<<blocks, dim3(32, 32), sizeof(float) * 32 * 32>>>(
-            x_new, x, b, a, c, N
-        );
+            x_new, x, b, a, c, N);
+
+        err = cudaGetLastError();
+        CUDA_ERR_CHECK(err);
+
+        switch (e)
+        {
+        case BoundaryHandleEnum::HandleRho:
+            handle_rho_bounderies_device<<<blocks_for_bd_task, dim3(128)>>>(x_new, N);
+            break;
+        case BoundaryHandleEnum::HandleU:
+            handle_u_bounderies_device<<<blocks_for_bd_task, dim3(128)>>>(x_new, N);
+            break;
+        case BoundaryHandleEnum::HandleV:
+            handle_v_bounderies_device<<<blocks_for_bd_task, dim3(128)>>>(x_new, N);
+            break;
+        default:
+            break;
+        }
+
+        err = cudaGetLastError();
+        CUDA_ERR_CHECK(err);
+
+        std::swap(x_new, x);
     }
 }
 
--- inc/fluid_gpu.hpp
+++ inc/fluid_gpu.hpp
@@ -25,9 +25,22 @@ public:
     }
 
 private:
+    enum class BoundaryHandleEnum
+    {
+        HandleU,
+        HandleV,
+        HandleRho
+    };
+
+    void diffuse_density(float dt);
+    void diffuse_velocity(float dt);
+    void project(float dt);
+    void advect_density(float dt);
+    void advect_velocity(float dt);
+
     void solve_laplace_eq_JA_solver(cudaSurfaceObject_t x_new,
                                     cudaSurfaceObject_t x, cudaSurfaceObject_t b,
-                                    float a, float c);
+                                    float a, float c, enum BoundaryHandleEnum e);
     void prepare_surface(cudaResourceDesc *desc, cudaSurfaceObject_t *surf, cudaArray_t *arr);
 
     /* */
@@ -38,7 +51,8 @@ private:
     cudaResourceDesc u_desc;
     cudaResourceDesc v_desc;
 
-    cudaResourceDesc temp_desc;
+    cudaResourceDesc x_desc;
+    cudaResourceDesc x_new_desc;
 
     cudaResourceDesc divergence_desc;
     cudaResourceDesc p_desc;
@@ -48,20 +62,22 @@ private:
     cudaSurfaceObject_t u_surf;
     cudaSurfaceObject_t v_surf;
 
-    cudaSurfaceObject_t temp_surf;
+    cudaSurfaceObject_t x_surf;
+    cudaSurfaceObject_t x_new_surf;
 
     cudaSurfaceObject_t divergence_surf;
     cudaSurfaceObject_t p_surf;
 
     /* Array handles */
-    cudaArray_t *density_arr;
-    cudaArray_t *u_arr;
-    cudaArray_t *v_arr;
+    cudaArray_t density_arr;
+    cudaArray_t u_arr;
+    cudaArray_t v_arr;
 
-    cudaArray_t *temp_arr;
+    cudaArray_t x_arr;
+    cudaArray_t x_new_arr;
 
-    cudaArray_t *divergence_arr;
-    cudaArray_t *p_arr;
+    cudaArray_t divergence_arr;
+    cudaArray_t p_arr;
 
     /* Fluid params */
     size_t N;
